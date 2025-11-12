8544ef375472efc40e2e9189bca13cdb611abe5c
Administrator <Administrator@AAST-IDC>
2025-11-12 11:20:44 +0200

    Add TA evaluation feature with RTL and modal updates
    Introduced a new `Professor.razor` component for managing
    teaching assistant evaluations, including a dynamic modal
    for grading. Added `TAEvaluationDto` to represent evaluation
    data. Enabled RTL support with `Custom-Rtl.css` and updated
    modal design for reusability.
    
    Enhanced accessibility, error handling, and loading states.
    Implemented dynamic total calculation and grade clamping.

---
+++ Shared/Dtos/TAEvaluationDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public class TAEvaluationDto
+    {
+            public int Id { get; set; }               
+            public string TAName { get; set; } = "";
+            public string WorkId { get; set; } = "";
+            public string SubjectName { get; set; } = "";
+            public int NumberOfClasses { get; set; }  
+            
+
+            public int? Total { get; set; }
+
+            public string Status
+            {
+                get
+                {
+                    if (Total.HasValue) return $"{Total}";
+                    else return"تقييم";
+                }
+            }
+        }
+}
